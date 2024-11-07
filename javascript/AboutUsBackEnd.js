38f10aafdeefa0c426dbfc339ba92607d750b871
Alexander B-P <98350761+Alexander006@users.noreply.github.com>
2024-11-07 12:37:56 -0500

    Initial Commit

---
+++ javascript/AboutUsBackEnd.js
@@ -0,0 +1,9 @@
+
+
+function RenderAboutUs(){
+
+}
+
+function Navigate(){
+    
+}
---
+++ javascript/enums/CurrentPage.js
@@ -0,0 +1,10 @@
+//Allows easy management of which page to render on screen
+//----------------------------------------------------------------
+const CurrentPage = {
+    MainPage : "MainPage.js",
+    AboutUsPage: "AboutUsPage.js",
+    HaitiNewsPage : "HaitiNewsPage.js",     
+}
+
+
+export default CurrentPage
